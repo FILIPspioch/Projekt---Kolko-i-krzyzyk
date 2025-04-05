b879e1162d720917b7922bb5554749562e44ba36
FilipSpioch <biedaczeq@gmail.com>
2025-04-05 13:17:20 +0200

    dodanie sprawdzania czy wygrywa znak

--- JS/script.js
+++ JS/script.js
@@ -17,12 +17,15 @@ class boxObj {
 }
 
 //tablice
-let boxesObjects = new Array(8);
+let boxesObjects = new Array(9);
 //kod
 btn_player1.addEventListener("click", () => (activePlayer = "player1"));
 btn_player2.addEventListener("click", () => (activePlayer = "player2"));
 
 for (let i = 0; i < boxes.length; i++) {
+  // Tworzymy obiekt, bo nie był stworzony wcześniej
+  boxesObjects[i] = new boxObj(false, undefined);
+
   boxes[i].addEventListener("click", (e) => {
     //tu kod co jak kliknieto
     if (activePlayer === "player1") {
@@ -37,24 +40,110 @@ for (let i = 0; i < boxes.length; i++) {
       }
     }
 
-    checkForWin();
+    console.log(checkForWin("X"));
+    console.log(checkForWin("O"));
   });
 }
 
 function funPlayX(boxIndex) {
-  let obj = new boxObj("true", "X");
-  boxesObjects[boxIndex] = obj;
+  // To zmienione bo nie ma sensu deklarować od nowa
+  boxesObjects[boxIndex].occupied = true;
+  boxesObjects[boxIndex].char = "X";
   boxes[boxIndex].innerHTML = "<p>X<p>";
 }
 
 function funPlayCircle(boxIndex) {
-  let obj = new boxObj("true", "O");
-  boxesObjects[boxIndex] = obj;
+  boxesObjects[boxIndex].occupied = true;
+  boxesObjects[boxIndex].char = "O";
   boxes[boxIndex].innerHTML = "<p>O<p>";
 }
 
-function checkForWin() {
-  //if()
+function checkForWin(symbol) {
+  //tutaj sprawdzac indexy
+  if (
+    // linia pozioma 1
+    boxesObjects[0].occupied == true &&
+    boxesObjects[0].char === symbol &&
+    boxesObjects[1].occupied == true &&
+    boxesObjects[1].char == symbol &&
+    boxesObjects[2].occupied == true &&
+    boxesObjects[2].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else if (
+    //linia pozioma 2
+    boxesObjects[3].occupied == true &&
+    boxesObjects[3].char == symbol &&
+    boxesObjects[4].occupied == true &&
+    boxesObjects[4].char == symbol &&
+    boxesObjects[5].occupied == true &&
+    boxesObjects[5].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else if (
+    boxesObjects[6].occupied == true &&
+    boxesObjects[6].char == symbol &&
+    boxesObjects[7].occupied == true &&
+    boxesObjects[7].char == symbol &&
+    boxesObjects[8].occupied == true &&
+    boxesObjects[8].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else if (
+    // linia skośna 1
+    boxesObjects[0].occupied == true &&
+    boxesObjects[0].char == symbol &&
+    boxesObjects[4].occupied == true &&
+    boxesObjects[4].char == symbol &&
+    boxesObjects[8].occupied == true &&
+    boxesObjects[8].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else if (
+    //linia skośna 2
+    boxesObjects[2].occupied == true &&
+    boxesObjects[2].char == symbol &&
+    boxesObjects[4].occupied == true &&
+    boxesObjects[4].char == symbol &&
+    boxesObjects[6].occupied == true &&
+    boxesObjects[6].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else if (
+    //linia pionowa 1
+    boxesObjects[0].occupied == true &&
+    boxesObjects[0].char == symbol &&
+    boxesObjects[3].occupied == true &&
+    boxesObjects[3].char == symbol &&
+    boxesObjects[6].occupied == true &&
+    boxesObjects[6].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else if (
+    // linia pionowa 2
+    boxesObjects[1].occupied == true &&
+    boxesObjects[1].char == symbol &&
+    boxesObjects[4].occupied == true &&
+    boxesObjects[4].char == symbol &&
+    boxesObjects[7].occupied == true &&
+    boxesObjects[7].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else if (
+    //linia pion 3
+    boxesObjects[2].occupied == true &&
+    boxesObjects[2].char == symbol &&
+    boxesObjects[5].occupied == true &&
+    boxesObjects[5].char == symbol &&
+    boxesObjects[8].occupied == true &&
+    boxesObjects[8].char == symbol
+  ) {
+    return `${symbol} wins`;
+  } else {
+    return `no wins`;
+  }
 }
 
-function isOccupied(boxIndex) {}
+function isOccupied(boxIndex) {
+  return boxesObjects[boxIndex].occupied;
+}
